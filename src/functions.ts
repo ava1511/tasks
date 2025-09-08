4a66c7e8d6e8c7c5176e89739e10405233f33d0f
ava1511 <avanat@udel.edu>
2025-09-08 11:23:02 -0400

    functions.ts isQuestion function completed

--- src/functions.ts
+++ src/functions.ts
@@ -34,7 +34,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let lastChar: string = message.slice(-1);
+    return lastChar === "?";
 }
 
 /**
