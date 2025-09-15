edf98140599edc81a891259b9c76eac23670858a
ava1511 <avanat@udel.edu>
2025-09-15 15:58:30 -0400

    completed makeBlankQuestion, isCorrect, isValid functions

--- src/objects.ts
+++ src/objects.ts
@@ -8,9 +8,18 @@ import { Question, QuestionType } from "./interfaces/question";
 export function makeBlankQuestion(
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question {
-    return {};
+    return {
+        id,
+        name,
+        type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,6 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
+    const trimmedAnswer = answer.trim();
+    if (trimmedAnswer.toLowerCase() === question.expected.toLowerCase())
+        return true;
     return false;
 }
 
@@ -31,7 +43,12 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    let valid: boolean = false;
+    if (question.type === "short_answer_question") valid = true;
+    else {
+        valid = question.options.includes(answer);
+    }
+    return valid;
 }
 
 /**
@@ -115,7 +132,7 @@ export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-    { points }: { points: number }
+    { points }: { points: number },
 ): Question {
     return contentQuestion;
 }
