056048851e9b90d4cdde009c8fc7dbba25d39616
ava1511 <avanat@udel.edu>
2025-09-25 08:14:46 -0400

    all components completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
-// import { CycleHoliday } from "./components/CycleHoliday";
+import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -23,8 +23,8 @@ function App(): React.JSX.Element {
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
-            {/* <hr /> */}
-            {/* <CycleHoliday></CycleHoliday> */}
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,37 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+/*Halloween, Christmas, Thanksgiving, Fourth of July, Easter*/
+type Holiday = "🎃" | "🎄" | "🦃" | "🎆" | "🐰";
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🎃");
+
+    function makeAlphabetical(): void {
+        setHoliday(
+            holiday === "🎄" ? "🐰"
+            : holiday === "🐰" ? "🎆"
+            : holiday === "🎆" ? "🎃"
+            : holiday === "🎃" ? "🦃"
+            : "🎄",
+        );
+    }
+
+    function makeChronological(): void {
+        setHoliday(
+            holiday === "🐰" ? "🎆"
+            : holiday === "🎆" ? "🎃"
+            : holiday === "🎃" ? "🦃"
+            : holiday === "🦃" ? "🎄"
+            : "🐰",
+        );
+    }
+
+    return (
+        <div>
+            <Button onClick={makeAlphabetical}>Advance By Alphabet</Button>
+            <Button onClick={makeChronological}>Advance By Year</Button>
+            Holiday: {holiday}
+        </div>
+    );
 }
