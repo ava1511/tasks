c37a67a534c62196def133366ea73b6743bfca8e
ava1511 <avanat@udel.edu>
2025-10-01 16:36:08 -0400

    CheckAnswer and GiveAttempts components done

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function checkMatch(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={answer}
+                    onChange={checkMatch}
+                />
+            </Form.Group>
             <h3>Check Answer</h3>
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,52 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<string>("");
+
+    const requestedToNum = parseInt(attemptsRequested);
+
+    let buttonDisabled = false;
+    if (attemptsLeft === 0) buttonDisabled = true;
+
     return (
         <div>
+            <Form.Group controlId="requestedAttempts">
+                <Form.Label>Request Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setAttemptsRequested(event.target.value);
+                    }}
+                />
+            </Form.Group>
             <h3>Give Attempts</h3>
+            <div>
+                <p>Current # of attempts: {attemptsLeft}</p>
+
+                <div>
+                    <Button
+                        disabled={buttonDisabled}
+                        onClick={() => {
+                            setAttemptsLeft(attemptsLeft - 1);
+                        }}
+                    >
+                        Use
+                    </Button>
+
+                    <Button
+                        onClick={() => {
+                            if (!isNaN(requestedToNum)) {
+                                setAttemptsLeft(attemptsLeft + requestedToNum);
+                            }
+                        }}
+                    >
+                        Gain
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
